368d67e27d20666f30d8222edbfc91e80ba3103d
Ivã Munhoz <iamunhoz@gmail.com>
2024-02-07 14:54:34 -0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Layout from "./layout";
-import { usePageRoutes } from "./pages";
-import Login from "./pages/Login";
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Layout from "./components/layout"
+import { usePageRoutes } from "./pages"
+import Login from "./pages/Login"
 
 function App() {
- const { pageRoutes } = usePageRoutes();
- const router = createBrowserRouter([
-   {
-     path: "/",
-     element: <Layout />,
-     children: pageRoutes,
-     errorElement: <main>Error</main>,
-   },
-   {
-     path: "/login",
-     element: <Login />,
-     errorElement: <main>Error</main>,
-   },
- ]);
- return <RouterProvider router={router} />;
+  const { pageRoutes } = usePageRoutes()
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: pageRoutes,
+      errorElement: <main>Error</main>,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+      errorElement: <main>Error</main>,
+    },
+  ])
+  return <RouterProvider router={router} />
 }
 
 export default App
