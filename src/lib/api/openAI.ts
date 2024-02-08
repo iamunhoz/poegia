32bafe243bf0656d3a3b5e0f2c311cb1b93bf77e
Ivã Munhoz <iamunhoz@gmail.com>
2024-02-08 11:42:32 -0300

    openai createvariation fn

--- src/lib/api/openAI.ts
+++ src/lib/api/openAI.ts
@@ -1,24 +1,44 @@
-import OpenAI from "openai";
+import OpenAI from "openai"
+import woodenImage from "src/lib/assets/wooden-wall-cropped.png"
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true
-});
+  dangerouslyAllowBrowser: true,
+})
 
-export const openaiImageQuery = async (prompt:string) => {
-    try {
-        const response = await openai.images.generate({
-            prompt,
-            model: 'dall-e-3',
-            n: 1,
-            size: '1024x1024'
-        })
+export const openaiImageQuery = async (prompt: string) => {
+  try {
+    const response = await openai.images.generate({
+      prompt,
+      model: "dall-e-3",
+      n: 1,
+      size: "1024x1024",
+    })
 
-        console.log({ response })
-    
-        return response.data[0].url || null
-    } catch (error) {
-        console.error({error})
-        return null
-    }
+    console.log({ response })
+
+    return response.data[0].url || null
+  } catch (error) {
+    console.error({ error })
+    return null
+  }
+}
+
+export const openaiImageEdit = async () => {
+  try {
+    const response = await openai.images.createVariation({
+      image: await fetch(woodenImage),
+
+      //   model: "dall-e-3",
+      n: 1,
+      size: "1024x1024",
+    })
+
+    console.log({ response })
+
+    return response.data[0].url || null
+  } catch (error) {
+    console.error({ error })
+    return null
+  }
 }
